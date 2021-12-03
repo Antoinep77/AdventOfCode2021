ed5718ebffb214d7613bc269d7512e7581a19893
Antoine Paris <antoineparis77@gmail.com>
2021-12-03 22:44:49 +0100

    improve day1 syntax

--- src/Day1/Exo1.hs
+++ src/Day1/Exo1.hs
@@ -1,10 +1,11 @@
 module Day1.Exo1
-    ( solution
-    ) where
-        
+  ( solution,
+  )
+where
+
 solution :: String -> Int
-solution = foldl (+) 0 . map fromEnum . mapIncreased . fmap read . words
+solution = sum . map fromEnum . mapIncreased . map read . words
 
 mapIncreased :: [Int] -> [Bool]
-mapIncreased (h1:h2:t) =(h1 < h2) :  (mapIncreased (h2:t))
+mapIncreased (h1 : h2 : t) = (h1 < h2) : mapIncreased (h2 : t)
 mapIncreased _ = []
